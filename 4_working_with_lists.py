831abc7017aea32ba0cdf6eb961b59a3cb6bc613
SauliusLuk <bendrasinbox-4@yahoo.com>
2022-09-18 18:44:21 +0300

    adding more exercises

--- 4_working_with_lists.py
+++ 4_working_with_lists.py
@@ -55,10 +55,48 @@
 # cubes = [ cube ** 3 for cube in range(1,10)]
 # print(cubes)
 
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[1:3])
-print(players[:4])
-print(players[2:])
-print(players[-3:])
-for player in players[:3]:
-    print(player)
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[1:3])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
+# print(f"\nThere are three players on my team:")
+# for player in players[:3]:
+#     print(player.title())
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+# my_foods.append('apple')
+# friend_foods.append('ice cream')
+#
+# print(my_foods)
+# print(friend_foods)
+
+# 4-10
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# print(f"the first three items on the list are {my_foods[:2]}")
+# print(f"The items in the middle are {my_foods[1:2]}")
+# print(f"The last item is {my_foods[2:]}")
+
+# 4-11
+# pizzas = ['pepperoni', 'mozzarella', 'hawaiian', 'four cheese']
+# friends_pizzas = pizzas[:]
+# pizzas.append('diavola')
+# friends_pizzas.append('chicken')
+# print(f"my favourite pizzas are:")
+# for pizza in pizzas:
+#     print(pizza)
+#
+# print(f"my friends favourite pizzas are:")
+# for pizza in friends_pizzas:
+#     print(pizza)
+
+# 4-12
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_foods = my_foods[:]
+friends_foods.append('ice cream')
+for food in my_foods:
+    print(food)
+print(f"my friends favourite foods are:")
+for food in friends_foods:
+    print(food)
