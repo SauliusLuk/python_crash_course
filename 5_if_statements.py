5670e07988df1bd19ed367af19a01b690ba0ec71
SauliusLuk <bendrasinbox-4@yahoo.com>
2022-09-29 17:14:27 +0300

    synching

---
+++ 5_if_statements.py
@@ -0,0 +1,92 @@
+# # A simple example
+# cars =  ['audi', 'bmw', 'subaru', 'toyota']
+#
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# # conditional tests
+# car = 'bmw'
+# print(bool(car))
+# print(bool(car == 'bmw'))
+# print(car == 'Bmw') # Case is important
+
+# # Checking for Inequality
+# requested_toppings = 'mushrooms'
+#
+# if requested_toppings != 'anchovies':
+#     print('Hold the anchovies!')
+
+# # magic_number.py
+# answer = 17
+#
+# if answer != 42:
+#     print("Incorrect answer")
+
+# # Checking Whether a Value Is in a List
+# requested_toppings = ['mushrooms', 'onions', 'pineapple']
+# print('mushrooms' in requested_toppings)
+# print('pepperoni' in requested_toppings)
+
+# # banned_users.py Checking Whether a Value Is Not in a List
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+#
+# if user not in banned_users:
+#     print(f"User {user} is not in the banned list")
+
+# # 5-1. Conditional Tests
+# fruit = 'apple'
+# print(f"Is fruit == 'apple? I predict True")
+# print(fruit == 'apple')
+#
+# fruit = 'orange'
+# print(f"Is fruit == 'apple? I predict False")
+# print(fruit == 'apple')
+#
+# fruit = 'plum'
+# print(fruit != 'orange')
+#
+# fruit = 'orange'
+# print(fruit == 'orange')
+#
+# fruit = 'orange'
+# print(fruit != 'orange')
+#
+# fruit = 'pear'
+# print(fruit == 'pear')
+#
+# fruit = 'pear'
+# print(fruit != 'pear')
+#
+# fruit = 'watermellon'
+# print(fruit == 'watermellon')
+#
+# fruit = 'watermellon'
+# print(fruit == 'orange')
+#
+# fruit = 'watermellon'
+# print(fruit != 'watermellon')
+
+# # 5-2. More Conditional Tests
+# car = 'Bmw'
+# print(car == 'bmw')
+# print(car.lower() == 'bmw')
+#
+# number = 8
+# print(number == 7)
+# print(number >= 6 )
+# print(number <= 5)
+# print(number >= 8)
+# print(number > 6 and number % 2 == 0)
+# print(number > 9 or number % 2 == 0)
+#
+# numbers = [1, 2, 3, 5, 9]
+# number = 5
+# print(number in numbers)
+# number_2 = 7
+# print(number_2 not in numbers)
+
+# if Statements
