de7c3ad2f465568cc9c61fe73125613764e067a5
Saulius Lukosius <bendrasinbox-4@yahoo.com>
2022-11-28 17:33:47 +0200

    Created requirements file

---
+++ 5_voting.py
@@ -0,0 +1,9 @@
+age = 17
+if age >= 18:
+    print("You are old enough to vote")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry you're too young to vote")
+    print("Come back later")
+    
+
