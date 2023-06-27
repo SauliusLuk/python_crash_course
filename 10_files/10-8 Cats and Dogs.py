0634a3393a0e99470466f9ddb0c4a2b305aa91f7
SauliusLuk <bendrasinbox-4@yahoo.com>
2023-06-27 13:34:12 +0300

    updated 10-8, practice

--- 10_files/10-8 Cats and Dogs.py
+++ 10_files/10-8 Cats and Dogs.py
@@ -12,3 +12,13 @@ filenames = ['cats.txt', 'dogs.txt']
 for filename in filenames:
     cats_dogs(filename)
 
+# Alternative
+files = ['cats.txt', 'dogs.txt']
+
+for file in files:
+    try:
+        with open(file, encoding='utf-8') as f:
+            contents = f.read()
+            print(f"Here's a content of the {file} file:\n{contents}")
+    except FileNotFoundError:
+        print(f"the file {file} not found")
