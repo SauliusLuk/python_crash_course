4b9f543b3189f5a516d14c5432ef299908e958d4
Saulius Lukosius <bendrasinbox-4@yahoo.com>
2023-07-12 17:50:02 +0300

    12-5 keys exercise

--- 12_pygame/keys.py
+++ 12_pygame/keys.py
@@ -17,11 +17,14 @@ class EmptyScreen:
         self.message_right = self.font.render("Right arrow key press detected", True, (255, 255, 255))
         self.message_left = self.font.render("Left arrow key press detected", True, (255, 255, 255))
 
+        self.keydown_detected = False
+
     def run_screen(self):
         while True:
             self._check_events()
             self.screen.fill(self.settings.bg_color)
-            self._blit_messages() # Blit messages onto the screen
+            if self.keydown_detected:
+                self._blit_messages() # Blit messages onto the screen
             pygame.display.flip()
 
 
@@ -32,15 +35,19 @@ class EmptyScreen:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     print("Right arrow key press detected")
+                    self.keydown_detected = True
                 elif event.key == pygame.K_LEFT:
                     print("Left arrow key press detected")
+                    self.keydown_detected = True
 
     def _blit_messages(self):
         message_right_rect = self.message_right.get_rect()
         message_left_rect = self.message_left.get_rect()
 
-        message_right_rect.center = (self.settings.screen_width // 2, self.settings.screen_height // 2 - message_right_rect.height)
-        message_left_rect = (self.settings.screen_width // 2, self.settings.screen_height // 2 + message_left_rect.height)
+        message_right_rect.center = (
+            self.settings.screen_width // 2, self.settings.screen_height // 2 - message_right_rect.height)
+        message_left_rect = (
+            self.settings.screen_width // 2, self.settings.screen_height // 2 + message_left_rect.height)
 
         self.screen.blit(self.message_right, message_right_rect) # Blit the messages onto the screen at desired positions
         self.screen.blit(self.message_left, message_left_rect)
