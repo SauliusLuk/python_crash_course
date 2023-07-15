81bee9252f14d96b952721bf7b534ea9f8539b86
SauliusLuk <bendrasinbox-4@yahoo.com>
2023-07-15 18:03:42 +0300

    practicing classes (9-5)

--- 9-classes/practice_classes.py
+++ 9-classes/practice_classes.py
@@ -18,9 +18,11 @@ class Restaurant:
     def increment_numbers_served(self, numbers):
         self.numbers_served += numbers
 
+class IceCreamStand(Restaurant):
 
-
-
+    def __init__(self, name, cuisine, flavors=""):
+        super().__init__(name, cuisine)
+        self.flavors = flavors
 
 
 restraurant = Restaurant('Dera', "African")
@@ -31,3 +33,8 @@ restraurant.numbers_served = 9
 restraurant.set_numbers_served()
 restraurant.increment_numbers_served(9)
 restraurant.set_numbers_served()
+
+flavors = ["banana", "chocolate"]
+stand = IceCreamStand(f"('Goro', 'Italian Ice Cream', {flavors}))
+
+print(stand.describe_restaurant())
